4fdd975faf9832cd00c9d2989b7b675d84b04859
thanhgiau2604 <thanhgiau2604@gmail.com>
2022-12-04 19:28:11 +0700

    setup github action

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,7 +3,7 @@ import React from "react";
 const HomePage = () => {
   return (
     <div className="app">
-      <h1>HOME PAGE</h1>
+      <h1>HOME PAGE - 2</h1>
       <a href="/login">Go to login</a>
       <a href="/signup">Go to signup</a>
     </div>
